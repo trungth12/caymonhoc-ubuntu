9f62098d3eec4e95fdee7dcb3dd7d066e6b50962
trungth12 <trungth@hpu.edu.vn>
2017-08-21 09:08:34 +0700

    Auto start ubuntu 16.04

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
     redis   = require('redis'),
     client  = redis.createClient(6382),
     rest    = require('restler'),
-    port    = parseInt(process.env.PORT, 10) || 8181,
+    port    = parseInt(process.env.PORT, 10) || 8282,
 	  day 	= 24 * 3600;
     bodyParser = require('body-parser'),
     serveStatic = require('serve-static'),
@@ -15,37 +15,37 @@
   app.use(serveStatic(__dirname + '/public'));
   app.engine('html', require('ejs').renderFile);
   app.use(errorHandler({
-    dumpExceptions: true, 
+    dumpExceptions: true,
     showStack: true
   }));
-  
+
 //});
 
-app.get("/get/:id", function(req, res){  
+app.get("/get/:id", function(req, res){
   var id = req.params.id;
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.headers.host;
 console.log('request: ' + id);
 client.get(id, function(err, rid){
 	if (!rid) {
-     rest.get('http://localhost:3001/' + ip + '/'+ id).on('complete', function(data) {    
+     rest.get('http://localhost:3001/' + ip + '/'+ id).on('complete', function(data) {
         if (data.error) {
 		      client.set(id, JSON.stringify(data));
 				client.expire(id, day);
         	  res.end(JSON.stringify(data));
-		
+
         } else {
-			
+
       		client.set(id, JSON.stringify(data));
       		client.expire(id, day);
-      		res.end(JSON.stringify(data)); 
+      		res.end(JSON.stringify(data));
 		}
     });
    }
-   else {   
+   else {
 		client.expire(id, day);
 		res.end(JSON.stringify(JSON.parse(rid)));
-   }       	
-});  
+   }
+});
 });
 app.get("/checkltn/:id", function(req, res){
   var id = req.params.id;
@@ -55,23 +55,23 @@ app.get("/checkltn/:id", function(req, res){
   client.get(ckey, function(err, cid){
     if (!cid) {
     rest.get('http://localhost:3001/checkltn/' + ip + '/'+ id).on('complete', function(data) {
-        if (data.error) {     
+        if (data.error) {
       client.set(ckey, JSON.stringify(data));
-      client.expire(ckey, day);      
+      client.expire(ckey, day);
           res.end(JSON.stringify(data));
-        } else {      
+        } else {
           client.set(ckey, JSON.stringify(data));
-      client.expire(ckey, day);      
+      client.expire(ckey, day);
           res.end(JSON.stringify(data));
         }
-      
+
     });
-  } else {  
-    client.expire(ckey, day);      
+  } else {
+    client.expire(ckey, day);
     res.end(JSON.stringify(JSON.parse(cid)));
   }
   });
-  
+
 });
 app.get("/checktn/:id", function(req, res){
   var id = req.params.id;
@@ -81,23 +81,23 @@ app.get("/checktn/:id", function(req, res){
   client.get(ckey, function(err, cid){
     if (!cid) {
     rest.get('http://localhost:3001/checktn/' + ip + '/'+ id).on('complete', function(data) {
-        if (data.error) {     
+        if (data.error) {
       client.set(ckey, JSON.stringify(data));
-      client.expire(ckey, day);      
+      client.expire(ckey, day);
           res.end(JSON.stringify(data));
-        } else {      
+        } else {
           client.set(ckey, JSON.stringify(data));
-      client.expire(ckey, day);      
+      client.expire(ckey, day);
           res.end(JSON.stringify(data));
         }
-      
+
     });
-  } else {  
-    client.expire(ckey, day);      
+  } else {
+    client.expire(ckey, day);
     res.end(JSON.stringify(JSON.parse(cid)));
   }
   });
-  
+
 });
 
 app.get("/check/:id", function(req, res){
@@ -106,25 +106,25 @@ app.get("/check/:id", function(req, res){
 	client.get(ckey, function(err, cid){
 		if (!cid) {
 		rest.get('http://localhost:3001/check/' + id).on('complete', function(data) {
-	      if (data.error) {			
+	      if (data.error) {
 			client.set(ckey, JSON.stringify(data));
-			client.expire(ckey, day);      
+			client.expire(ckey, day);
         	res.end(JSON.stringify(data));
-	      } else {		  
+	      } else {
         	client.set(ckey, JSON.stringify(data));
-			client.expire(ckey, day);      
+			client.expire(ckey, day);
 	      	res.end(JSON.stringify(data));
 	      }
-			
+
 		});
-	} else {	
-		client.expire(ckey, day);      
+	} else {
+		client.expire(ckey, day);
 		res.end(JSON.stringify(JSON.parse(cid)));
 	}
 	});
-	
+
 });
-app.get("/", function(req, res) {  
+app.get("/", function(req, res) {
 	process.send({ cmd: 'notify', src: process.pid });
   res.redirect("/index.html");
 });
--- start.bat
+++
@@ -1,3 +0,0 @@
-set RACK_ENV=production
-bundle exec puma config.ru -b tcp://0.0.0.0:3010 -t 4:32 RACK_ENV=%RACK_ENV%
-pause
